3339bdf575b97ae1d9de445f7ca5728c717b0ad3
max <fritzschoff@icloud.com>
2023-02-13 09:47:02 +0000

    updated subgraph

--- perps-subgraph/generated/schema.ts
+++ perps-subgraph/generated/schema.ts
@@ -157,15 +157,6 @@ export class Trader extends Entity {
     this.set("totalMarginLiquidated", Value.fromBigDecimal(value));
   }
 
-  get tradeCount(): BigInt {
-    let value = this.get("tradeCount");
-    return value!.toBigInt();
-  }
-
-  set tradeCount(value: BigInt) {
-    this.set("tradeCount", Value.fromBigInt(value));
-  }
-
   get feesPaidToSynthetix(): BigDecimal {
     let value = this.get("feesPaidToSynthetix");
     return value!.toBigDecimal();
@@ -173,6 +164,172 @@ export class Trader extends Entity {
 
   set feesPaidToSynthetix(value: BigDecimal) {
     this.set("feesPaidToSynthetix", Value.fromBigDecimal(value));
+  }
+
+  get pnl(): BigInt {
+    let value = this.get("pnl");
+    return value!.toBigInt();
+  }
+
+  set pnl(value: BigInt) {
+    this.set("pnl", Value.fromBigInt(value));
+  }
+
+  get trades(): Array<string> | null {
+    let value = this.get("trades");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set trades(value: Array<string> | null) {
+    if (!value) {
+      this.unset("trades");
+    } else {
+      this.set("trades", Value.fromStringArray(<Array<string>>value));
+    }
+  }
+}
+
+export class FuturesTrade extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save FuturesTrade entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type FuturesTrade must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("FuturesTrade", id.toString(), this);
+    }
+  }
+
+  static load(id: string): FuturesTrade | null {
+    return changetype<FuturesTrade | null>(store.get("FuturesTrade", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get timestamp(): BigInt {
+    let value = this.get("timestamp");
+    return value!.toBigInt();
+  }
+
+  set timestamp(value: BigInt) {
+    this.set("timestamp", Value.fromBigInt(value));
+  }
+
+  get account(): Bytes {
+    let value = this.get("account");
+    return value!.toBytes();
+  }
+
+  set account(value: Bytes) {
+    this.set("account", Value.fromBytes(value));
+  }
+
+  get margin(): BigInt {
+    let value = this.get("margin");
+    return value!.toBigInt();
+  }
+
+  set margin(value: BigInt) {
+    this.set("margin", Value.fromBigInt(value));
+  }
+
+  get size(): BigInt {
+    let value = this.get("size");
+    return value!.toBigInt();
+  }
+
+  set size(value: BigInt) {
+    this.set("size", Value.fromBigInt(value));
+  }
+
+  get asset(): string {
+    let value = this.get("asset");
+    return value!.toString();
+  }
+
+  set asset(value: string) {
+    this.set("asset", Value.fromString(value));
+  }
+
+  get market(): string {
+    let value = this.get("market");
+    return value!.toString();
+  }
+
+  set market(value: string) {
+    this.set("market", Value.fromString(value));
+  }
+
+  get price(): BigInt {
+    let value = this.get("price");
+    return value!.toBigInt();
+  }
+
+  set price(value: BigInt) {
+    this.set("price", Value.fromBigInt(value));
+  }
+
+  get positionSize(): BigInt {
+    let value = this.get("positionSize");
+    return value!.toBigInt();
+  }
+
+  set positionSize(value: BigInt) {
+    this.set("positionSize", Value.fromBigInt(value));
+  }
+
+  get positionClosed(): boolean {
+    let value = this.get("positionClosed");
+    return value!.toBoolean();
+  }
+
+  set positionClosed(value: boolean) {
+    this.set("positionClosed", Value.fromBoolean(value));
+  }
+
+  get pnl(): BigInt {
+    let value = this.get("pnl");
+    return value!.toBigInt();
+  }
+
+  set pnl(value: BigInt) {
+    this.set("pnl", Value.fromBigInt(value));
+  }
+
+  get feesPaidToSynthetix(): BigInt {
+    let value = this.get("feesPaidToSynthetix");
+    return value!.toBigInt();
+  }
+
+  set feesPaidToSynthetix(value: BigInt) {
+    this.set("feesPaidToSynthetix", Value.fromBigInt(value));
+  }
+
+  get txHash(): string {
+    let value = this.get("txHash");
+    return value!.toString();
+  }
+
+  set txHash(value: string) {
+    this.set("txHash", Value.fromString(value));
   }
 }
 
@@ -223,6 +380,15 @@ export class Synthetix extends Entity {
 
   set feesByPositionModifications(value: BigDecimal) {
     this.set("feesByPositionModifications", Value.fromBigDecimal(value));
+  }
+
+  get totalVolume(): BigDecimal {
+    let value = this.get("totalVolume");
+    return value!.toBigDecimal();
+  }
+
+  set totalVolume(value: BigDecimal) {
+    this.set("totalVolume", Value.fromBigDecimal(value));
   }
 }
 
@@ -298,6 +464,15 @@ export class FuturesPosition extends Entity {
     this.set("id", Value.fromString(value));
   }
 
+  get account(): Bytes {
+    let value = this.get("account");
+    return value!.toBytes();
+  }
+
+  set account(value: Bytes) {
+    this.set("account", Value.fromBytes(value));
+  }
+
   get openTimestamp(): BigInt {
     let value = this.get("openTimestamp");
     return value!.toBigInt();
@@ -324,13 +499,13 @@ export class FuturesPosition extends Entity {
     }
   }
 
-  get block(): BigInt {
-    let value = this.get("block");
-    return value!.toBigInt();
+  get long(): boolean {
+    let value = this.get("long");
+    return value!.toBoolean();
   }
 
-  set block(value: BigInt) {
-    this.set("block", Value.fromBigInt(value));
+  set long(value: boolean) {
+    this.set("long", Value.fromBoolean(value));
   }
 
   get market(): string {
@@ -414,13 +589,13 @@ export class FuturesPosition extends Entity {
     this.set("pnl", Value.fromBigInt(value));
   }
 
-  get totalDeposits(): BigInt {
-    let value = this.get("totalDeposits");
+  get totalVolume(): BigInt {
+    let value = this.get("totalVolume");
     return value!.toBigInt();
   }
 
-  set totalDeposits(value: BigInt) {
-    this.set("totalDeposits", Value.fromBigInt(value));
+  set totalVolume(value: BigInt) {
+    this.set("totalVolume", Value.fromBigInt(value));
   }
 
   get entryPrice(): BigInt {
@@ -456,55 +631,5 @@ export class FuturesPosition extends Entity {
     } else {
       this.set("exitPrice", Value.fromBigInt(<BigInt>value));
     }
-  }
-}
-
-export class FuturesMarket extends Entity {
-  constructor(id: string) {
-    super();
-    this.set("id", Value.fromString(id));
-  }
-
-  save(): void {
-    let id = this.get("id");
-    assert(id != null, "Cannot save FuturesMarket entity without an ID");
-    if (id) {
-      assert(
-        id.kind == ValueKind.STRING,
-        `Entities of type FuturesMarket must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
-      );
-      store.set("FuturesMarket", id.toString(), this);
-    }
-  }
-
-  static load(id: string): FuturesMarket | null {
-    return changetype<FuturesMarket | null>(store.get("FuturesMarket", id));
-  }
-
-  get id(): string {
-    let value = this.get("id");
-    return value!.toString();
-  }
-
-  set id(value: string) {
-    this.set("id", Value.fromString(value));
-  }
-
-  get asset(): Bytes {
-    let value = this.get("asset");
-    return value!.toBytes();
-  }
-
-  set asset(value: Bytes) {
-    this.set("asset", Value.fromBytes(value));
-  }
-
-  get marketKey(): Bytes {
-    let value = this.get("marketKey");
-    return value!.toBytes();
-  }
-
-  set marketKey(value: Bytes) {
-    this.set("marketKey", Value.fromBytes(value));
   }
 }
--- perps-subgraph/src/futures.ts
+++ perps-subgraph/src/futures.ts
@@ -8,6 +8,7 @@ import {
   Trader,
   Synthetix,
   FuturesPosition,
+  FuturesTrade,
 } from '../generated/schema';
 import { PerpMarkets, PerpProxies } from '../utils/constants';
 
@@ -24,35 +25,50 @@ export function handlePositionLiquidated(event: PositionLiquidatedEvent): void {
   positionLiquidatedEntity.block = event.block.number;
   positionLiquidatedEntity.save();
 
-  let trader = Trader.load(event.params.account.toHex());
-  if (trader) {
-    trader.feesPaidToSynthetix = trader.feesPaidToSynthetix.plus(
-      event.params.fee.toBigDecimal()
-    );
-    trader.totalLiquidations = trader.totalLiquidations.plus(BigInt.fromI32(1));
-    trader.totalMarginLiquidated = trader.totalMarginLiquidated.plus(
-      event.params.size.toBigDecimal()
-    );
-    trader.save();
-  }
   let synthetix = Synthetix.load('synthetix');
-  if (!synthetix) {
-    synthetix = new Synthetix('synthetix');
-    synthetix.feesByLiquidations = event.params.fee.toBigDecimal();
-    synthetix.feesByPositionModifications = BigDecimal.fromString('0');
-  } else {
+  if (synthetix) {
     synthetix.feesByLiquidations = synthetix.feesByLiquidations.plus(
       event.params.fee.toBigDecimal()
     );
+
+    let futuresPosition = FuturesPosition.load(
+      event.address.toHex() + '-' + event.params.id.toHex()
+    );
+
+    if (futuresPosition) {
+      futuresPosition.isLiquidated = true;
+      futuresPosition.isOpen = false;
+      synthetix.totalVolume = synthetix.totalVolume.plus(
+        futuresPosition.totalVolume.toBigDecimal()
+      );
+      futuresPosition.save();
+    }
+    let trader = Trader.load(event.params.account.toHex());
+    if (trader) {
+      trader.feesPaidToSynthetix = trader.feesPaidToSynthetix.plus(
+        event.params.fee.toBigDecimal()
+      );
+      trader.totalLiquidations = trader.totalLiquidations.plus(
+        BigInt.fromI32(1)
+      );
+      trader.totalMarginLiquidated = trader.totalMarginLiquidated.plus(
+        event.params.size.toBigDecimal()
+      );
+      trader.save();
+    }
+    synthetix.save();
   }
-  synthetix.save();
 }
 
 export function handlePositionModified(event: PositionModifiedEvent): void {
+  // Load all information
   let futuresPosition = FuturesPosition.load(
     event.address.toHex() + '-' + event.params.id.toHex()
   );
+  let trader = Trader.load(event.params.account.toHex());
+  let synthetix = Synthetix.load('synthetix');
   let perpMarketInformation: PerpMarkets;
+
   for (let index = 0; index < PerpProxies.length; index++) {
     if (
       PerpProxies.at(index)
@@ -62,6 +78,20 @@ export function handlePositionModified(event: PositionModifiedEvent): void {
       perpMarketInformation = PerpProxies.at(index);
     }
   }
+
+  if (!synthetix) {
+    synthetix = new Synthetix('synthetix');
+    synthetix.feesByPositionModifications = event.params.fee.toBigDecimal();
+    synthetix.feesByLiquidations = BigDecimal.fromString('0');
+    synthetix.totalVolume = BigDecimal.fromString('0');
+    // If liquidated, we add the fees in the liquidation handler
+  } else if (!event.params.size.isZero() && !event.params.tradeSize.isZero()) {
+    synthetix.feesByPositionModifications = synthetix.feesByPositionModifications.plus(
+      event.params.fee.toBigDecimal()
+    );
+  }
+
+  // New position
   if (!futuresPosition) {
     futuresPosition = new FuturesPosition(
       event.address.toHex() + '-' + event.params.id.toHex()
@@ -73,84 +103,144 @@ export function handlePositionModified(event: PositionModifiedEvent): void {
       futuresPosition.asset = 'not found';
       futuresPosition.market = 'not found';
     }
-    futuresPosition.block = event.block.number;
     futuresPosition.openTimestamp = event.block.timestamp;
+    futuresPosition.account = event.params.account;
     futuresPosition.isOpen = true;
     futuresPosition.isLiquidated = false;
     futuresPosition.size = event.params.size;
     futuresPosition.initialMargin = event.params.margin.plus(event.params.fee);
     futuresPosition.margin = event.params.margin;
-    futuresPosition.pnl = BigInt.fromI32(0);
-    futuresPosition.totalDeposits = BigInt.fromI32(0);
+    futuresPosition.pnl = event.params.fee.times(BigInt.fromI32(-1));
     futuresPosition.entryPrice = event.params.lastPrice;
     futuresPosition.lastPrice = event.params.lastPrice;
+    futuresPosition.trades = BigInt.fromI32(1);
+    futuresPosition.long = event.params.tradeSize.gt(BigInt.fromI32(0));
+    futuresPosition.totalVolume = event.params.tradeSize
+      .times(event.params.lastPrice)
+      .div(BigInt.fromI32(10).pow(18))
+      .abs();
+
+    const tradeEntity = new FuturesTrade(
+      event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+    );
+    tradeEntity.timestamp = event.block.timestamp;
+    tradeEntity.account = event.params.account;
+    tradeEntity.margin = event.params.margin.plus(event.params.fee);
+    tradeEntity.size = event.params.tradeSize;
+    tradeEntity.asset = perpMarketInformation.getAsset();
+    tradeEntity.market = perpMarketInformation.getMarket();
+    tradeEntity.price = event.params.lastPrice;
+    tradeEntity.positionSize = event.params.size;
+    tradeEntity.pnl = event.params.fee.times(BigInt.fromI32(-1));
+    tradeEntity.feesPaidToSynthetix = event.params.fee;
+    tradeEntity.positionClosed = false;
+    tradeEntity.save();
+    if (!trader) {
+      trader = new Trader(event.params.account.toHex());
+      trader.totalLiquidations = BigInt.fromI32(0);
+      trader.totalMarginLiquidated = BigDecimal.fromString('0');
+      trader.feesPaidToSynthetix = event.params.fee.toBigDecimal();
+      trader.pnl = event.params.fee.times(BigInt.fromI32(-1));
+      trader.trades = [];
+    } else {
+      trader.feesPaidToSynthetix = trader.feesPaidToSynthetix.plus(
+        event.params.fee.toBigDecimal()
+      );
+    }
+    trader.trades!.push(tradeEntity.id);
   }
 
-  futuresPosition.trades = futuresPosition.trades.plus(BigInt.fromI32(1));
-
-  // calculate pnl
-  const newPnl = event.params.lastPrice
-    .minus(futuresPosition.lastPrice)
-    .times(futuresPosition.size)
-    .div(BigInt.fromI32(10).pow(18));
-  futuresPosition.pnl = futuresPosition.pnl.plus(newPnl);
-  if (event.params.size.isZero() == true) {
+  // Position closed & != liquidated
+  if (event.params.size.isZero() == true && !event.params.tradeSize.isZero()) {
     futuresPosition.isOpen = false;
     futuresPosition.exitPrice = event.params.lastPrice;
     futuresPosition.closeTimestamp = event.block.timestamp;
-  } else {
-    // if the position is not closed...
-    // if position changes sides, reset the entry price
-    if (
-      (futuresPosition.size.lt(BigInt.fromI32(0)) &&
-        event.params.size.gt(BigInt.fromI32(0))) ||
-      (futuresPosition.size.gt(BigInt.fromI32(0)) &&
-        event.params.size.lt(BigInt.fromI32(0)))
-    ) {
-      futuresPosition.entryPrice = event.params.lastPrice; // Deprecate this after migrating frontend
-    } else {
-      // check if the position side increases (long or short)
-      if (event.params.size.abs().gt(futuresPosition.size.abs())) {
-        // if so, calculate the new average price
-        const existingSize = futuresPosition.size.abs();
-        const existingPrice = existingSize.times(futuresPosition.entryPrice);
 
-        const newPrice = event.params.tradeSize
-          .abs()
-          .times(event.params.lastPrice);
-        futuresPosition.entryPrice = existingPrice
-          .plus(newPrice)
-          .div(event.params.size.abs()); // Deprecate this after migrating frontend
-      }
-      // otherwise do nothing
+    const tradeEntity = new FuturesTrade(
+      event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+    );
+
+    const newPnl = event.params.lastPrice
+      .minus(futuresPosition.lastPrice)
+      .times(futuresPosition.size)
+      .div(BigInt.fromI32(10).pow(18));
+
+    tradeEntity.timestamp = event.block.timestamp;
+    tradeEntity.account = event.params.account;
+    tradeEntity.margin = event.params.margin.plus(event.params.fee);
+    tradeEntity.size = event.params.tradeSize;
+    tradeEntity.asset = perpMarketInformation.getAsset();
+    tradeEntity.market = perpMarketInformation.getMarket();
+    tradeEntity.price = event.params.lastPrice;
+    tradeEntity.positionSize = event.params.size;
+    tradeEntity.pnl = newPnl;
+    tradeEntity.feesPaidToSynthetix = event.params.fee;
+    tradeEntity.positionClosed = true;
+    tradeEntity.save();
+    if (trader) {
+      // no need to add the fees to the trader entity because we are
+      // doing that in the liquidation event
+      trader.pnl = trader.pnl.plus(newPnl);
+      trader.trades!.push(tradeEntity.id);
+      trader.save();
     }
-  }
-  futuresPosition.size = event.params.size;
-  futuresPosition.margin = event.params.margin;
-  futuresPosition.lastPrice = event.params.lastPrice;
-  let trader = Trader.load(event.params.account.toHex());
-  if (!trader) {
-    trader = new Trader(event.params.account.toHex());
-    trader.totalLiquidations = BigInt.fromI32(0);
-    trader.totalMarginLiquidated = BigDecimal.fromString('0');
-    trader.tradeCount = BigInt.fromI32(1);
-    trader.feesPaidToSynthetix = event.params.fee.toBigDecimal();
-  } else {
-    trader.feesPaidToSynthetix = trader.feesPaidToSynthetix.plus(
-      event.params.fee.toBigDecimal()
+
+    // no need to add the fees to the synthetix entity because we are
+    // doing that in the liquidation event
+    synthetix.totalVolume = synthetix.totalVolume.plus(
+      futuresPosition.totalVolume.toBigDecimal()
     );
   }
-  let synthetix = Synthetix.load('synthetix');
-  if (!synthetix) {
-    synthetix = new Synthetix('synthetix');
-    synthetix.feesByPositionModifications = event.params.fee.toBigDecimal();
-    synthetix.feesByLiquidations = BigDecimal.fromString('0');
-  } else {
-    synthetix.feesByPositionModifications = synthetix.feesByPositionModifications.plus(
-      event.params.fee.toBigDecimal()
+
+  // If position is already opened, so it got modified
+  if (
+    !event.params.tradeSize.isZero() &&
+    !event.params.size.isZero() &&
+    futuresPosition
+  ) {
+    futuresPosition.long = event.params.size
+      .plus(event.params.tradeSize)
+      .gt(BigInt.fromI32(0));
+    const newPnl = event.params.lastPrice
+      .minus(futuresPosition.lastPrice)
+      .times(futuresPosition.size)
+      .div(BigInt.fromI32(10).pow(18));
+
+    let tradeEntity = new FuturesTrade(
+      event.transaction.hash.toHex() + '-' + event.logIndex.toString()
     );
+
+    tradeEntity.timestamp = event.block.timestamp;
+    tradeEntity.account = event.params.account;
+    tradeEntity.margin = event.params.margin.plus(event.params.fee);
+    tradeEntity.size = event.params.tradeSize;
+    tradeEntity.asset = perpMarketInformation.getAsset();
+    tradeEntity.market = perpMarketInformation.getMarket();
+    tradeEntity.price = event.params.lastPrice;
+    tradeEntity.positionSize = event.params.size;
+    tradeEntity.feesPaidToSynthetix = event.params.fee;
+    tradeEntity.positionClosed = false;
+    tradeEntity.pnl = newPnl;
+
+    tradeEntity.save();
+
+    if (trader) {
+      trader.feesPaidToSynthetix = trader.feesPaidToSynthetix.plus(
+        event.params.fee.toBigDecimal()
+      );
+      trader.pnl = trader.pnl.plus(newPnl);
+      trader.trades!.push(tradeEntity.id);
+      trader.save();
+    }
+
+    let volume = tradeEntity.size
+      .times(tradeEntity.price)
+      .div(BigInt.fromI32(10).pow(18))
+      .abs();
+
+    futuresPosition.totalVolume = futuresPosition.totalVolume.plus(volume);
   }
-  trader.save();
+
   synthetix.save();
   futuresPosition.save();
 }
